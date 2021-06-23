236344563db218155ead69595c7174897fcaaf39
Mehmet <mehmetkusken@gmail.com>
2021-06-22 20:52:09 -0600

    commit tree 238edae17da3dfc7516123a5d4ac41d5c2202cef

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,9 @@
 class City < ActiveRecord::Base
+  include FindableConcern::InstanceMethods
+  extend FindableConcern::ClassMethods 
+
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  has_many :reservations, through: :listings
 end
-
